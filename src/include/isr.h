a0bff88dbf8c4fecbd9e41ef998342b56c84c870
Saurabh <upscprep48@gmail.com>
2020-01-12 01:59:09 +0530

    With working keyboard and Timer. They dont do anything but are available

--- src/include/isr.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-typedef struct registers
-{
-   uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; // Pushed by pusha.
-   uint32_t int_no, err_code;    // Interrupt number and error code (if applicable)
-   uint32_t eip, cs, eflags, useresp, ss; // Pushed by the processor automatically.
-} registers_t;
