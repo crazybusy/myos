1760c4677ecd7d7306e15694912f9f6ce81e9c16
crazybusy <35750263+crazybusy@users.noreply.github.com>
2020-11-07 19:37:58 +0530

    Change folder structure
    Change folder structure for architecture support and split kernel into loader and main thread

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 
-ARCH_FOLDER="./src/kernel"
+ARCH_FOLDER="./src/intel"
 KERNEL_FOLDER="./src/kernel"
+BASE_FOLDER="./src"
+
 BUILD_FOLDER="./build"
 DIST_FOLDER="./dist"
 
@@ -20,15 +22,16 @@ $ASSEMBLER $ARCH_FOLDER/boot.s -o $BUILD_FOLDER/boot.o
 $COMPILER $COMPILER_PARAMS -I $ARCH_FOLDER/include -I $KERNEL_FOLDER/include $ARCH_FOLDER/descriptor_tables.c -o $BUILD_FOLDER/descriptor_tables.o 
 
 $ASSEMBLER $KERNEL_FOLDER/interrupts.s -o $BUILD_FOLDER/interrupts.o
+$COMPILER $COMPILER_PARAMS -I $ARCH_FOLDER/include -I $KERNEL_FOLDER/include $KERNEL_FOLDER/isr.c -o $BUILD_FOLDER/isr.o 
 $COMPILER $COMPILER_PARAMS -I $ARCH_FOLDER/include -I $KERNEL_FOLDER/include $KERNEL_FOLDER/devices.c -o $BUILD_FOLDER/devices.o 
 $COMPILER $COMPILER_PARAMS -I $ARCH_FOLDER/include -I $KERNEL_FOLDER/include $KERNEL_FOLDER/string.c -o $BUILD_FOLDER/string.o 
 $COMPILER $COMPILER_PARAMS -I $ARCH_FOLDER/include -I $KERNEL_FOLDER/include $KERNEL_FOLDER/tty.c -o $BUILD_FOLDER/tty.o 
-$COMPILER $COMPILER_PARAMS -I $ARCH_FOLDER/include -I $KERNEL_FOLDER/include $KERNEL_FOLDER/kernel.c -o $BUILD_FOLDER/kernel.o 
-$COMPILER $COMPILER_PARAMS -I $ARCH_FOLDER/include -I $KERNEL_FOLDER/include $KERNEL_FOLDER/isr.c -o $BUILD_FOLDER/isr.o 
 $COMPILER $COMPILER_PARAMS -I $ARCH_FOLDER/include -I $KERNEL_FOLDER/include $KERNEL_FOLDER/keyboard.c -o $BUILD_FOLDER/keyboard.o 
 
+$COMPILER $COMPILER_PARAMS -I $ARCH_FOLDER/include -I $KERNEL_FOLDER/include $BASE_FOLDER/kernel.c -o $BUILD_FOLDER/kernel.o 
+
 OBJECTS=$BUILD_FOLDER"/*"
-$LINKER $LINKER_PARAMS -T $KERNEL_FOLDER/linker.ld -o $DIST_FOLDER/myos.bin $OBJECTS
+$LINKER $LINKER_PARAMS -T $BASE_FOLDER/linker.ld -o $DIST_FOLDER/myos.bin $OBJECTS
 
 mkdir -p isodir/boot/grub
 cp $DIST_FOLDER/myos.bin isodir/boot/myos.bin
--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 ftp -s:getmyos.txt
 qemu-system-x86_64.exe -cdrom myos.iso
+REM "C:\Program Files\Oracle\VirtualBox\VBoxManage" startvm "Test"
